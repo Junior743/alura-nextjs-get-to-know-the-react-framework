06e7f6916d1e9317416e40da1fb4acfdcb5f83f5
Junior743 <carloshenriquesousajunior@gmail.com>
2025-11-03 05:03:54 -0300

    Add global styles and layout structure with Aside component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next'
+
 import './globals.css'
+
+import { Aside } from '@/components/Aside'
 
 export const metadata: Metadata = {
   title: 'NextJS',
@@ -13,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <div className="app-container">
+          <Aside />
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
